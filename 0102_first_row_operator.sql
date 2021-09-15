a4ea08d900b78b0516629d68eec40888ad437c3e
KhanhLVH <919300+lvhkhanh@users.noreply.github.com>
2021-09-15 12:15:30 +0700

    Create 0102_first_row_operator.sql

---
+++ 0102_first_row_operator.sql
@@ -0,0 +1,9 @@
+select min(employee_id) from employees where employee_id < 140;
+ 
+select max(employee_id) from employees where employee_id < 140;
+ 
+select min(employee_id) from employees;
+ 
+select min(employee_id), max(employee_id) from employees where employee_id < 140;
+ 
+select min(commission_pct) from employees where commission_pct < 0.1;
