a03a93ad18b8524c975e78f12a25f8e5fd9ca882
KhanhLVH <919300+lvhkhanh@users.noreply.github.com>
2021-09-18 10:21:19 +0700

    Create 0128_using_concatenation_operator.sql

---
+++ 0128_using_concatenation_operator.sql
@@ -0,0 +1,5 @@
+select first_name, last_name, employee_id, salary from employees 
+where first_name||last_name = 'StevenKING';
+ 
+select first_name, last_name, employee_id, salary from employees 
+where first_name = 'Steven' and last_name = 'KING';
